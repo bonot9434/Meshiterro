484d0502049210a45cb4ae8c39b402091aba64e3
bonot9434 <bonot9434@gmail.com>
2021-08-10 07:41:19 +0000

    [finish]meshiterro

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd65fb2e22c4561d3ab08e356f3e1c057e776df2da26f8bcd424be9a14b3e30ee778290936f13234cb7fd5873c6a3589d098f36fd5d886b93317824cf84d39052'
